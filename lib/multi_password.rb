17710eeb42d8009f5ed25006f36fdc6482285839
Hieu Nguyen <hieuk09@gmail.com>
2020-11-01 13:01:43 +0800

    Initial commit

---
+++ lib/multi_password.rb
@@ -0,0 +1,6 @@
+require "multi_password/version"
+
+module MultiPassword
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/multi_password/version.rb
@@ -0,0 +1,3 @@
+module MultiPassword
+  VERSION = "0.1.0"
+end
